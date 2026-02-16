29e3d47e221c0584df0ec83570c9b27fbe665cee
Rodrigo Motti de Santana <113243243+rmotti@users.noreply.github.com>
2026-02-16 18:09:38 -0300

    fix: test student route error

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ import db from './models/index.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
+//import swaggerUi from 'swagger-ui-express';
+//import swaggerJsdoc from 'swagger-jsdoc';
 
 import routes from './routes/index.js';
 
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+/*
 // --- SWAGGER CONFIG ---
 const swaggerOptions = {
   definition: {
@@ -54,8 +54,7 @@ const swaggerOptions = {
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-// --- DATABASE SYNC ---
+*/// --- DATABASE SYNC ---
 db.sequelize.sync()
     .then(() => {
         console.log("Database synced successfully.");
@@ -75,5 +74,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`API server is running on port http://localhost:${PORT}/`);
-  console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
+  //console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
 });
